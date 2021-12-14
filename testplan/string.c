167898a576b98d6e2b3ff7864cf110a9b2d80a48
Pitambar21 <94235986+Pitambar21@users.noreply.github.com>
2021-12-14 13:29:59 +0530

    Update string.c

--- testplan/string.c
+++ testplan/string.c
@@ -1,1 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+//#include<process.h>
+#include<string.h>
+void Arithmetic_Operators();
+void Suggestion();
+void Relational_Operators();
+void Logical_Operators();
+void Bitwise_Operators();
+void Operators_and_Expressions();
+int Index();
+void prgoao();
+void prgobo();
+void prgolo();
+void prgoro();
+void Arithmetic_Operators()
+      {
+	  system("cls");
+   	 FILE *p;
+	 char a[500];
+	 p=fopen("src/text_src/Arithmetic_Operators.txt","r");
+		while(!feof(p))
+		{
+		fgets(a,500,p);
+		printf("%s\n",a);
+		}
+   	 int n;    
+     printf("/npress 0 for chapter outline \n press 1 for next topic \n if you want to run this code press 2");
+     scanf("%d",&n);
+     if(n==0)
+    {system("cls");
+    	Operators_and_Expressions();
+	}
+    else if(n==1)
+	{
+		system("cls");
+		Relational_Operators();
+	}
+	else if (n==2)
+	{
+		system("cls");
+		prgoao();
+	}
+	fclose(p);
+	}//end of arithmetic operator
+	
+	void prgoao()
+	 {
 
+   int a = 21;
